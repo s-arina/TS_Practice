0b1b8004972ef0318d8bc520cbed25df955bec38
Sarina Chang <sarinachang7@gmail.com>
2023-01-08 22:38:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// Compnent adds metadata to the class AppComponent
+// it tells the module what its role is
 @Component({
-  selector: 'app-root',
+  // if selector is changed, the html tag in index.html must be as well
+  selector: 'app-welcome',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+// title is defined in {{ title }} in /app.component.html
 export class AppComponent {
   title = 'MyApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// NgModule adds metadata to the class AppModule
+// it tells the module what its role is
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap is the first component of the project
+  // only defined in the root module!!!
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
